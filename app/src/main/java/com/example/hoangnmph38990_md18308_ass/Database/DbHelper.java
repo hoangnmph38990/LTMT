ba4d7d8cf3b79f971e1ffee704fdf577813f893b
Ass <hoangnmph38990@fpt.edu.vn>
2023-09-20 16:30:02 +0700

    Final Commit

--- app/src/main/java/com/example/hoangnmph38990_md18308_ass/Database/DbHelper.java
+++ app/src/main/java/com/example/hoangnmph38990_md18308_ass/Database/DbHelper.java
@@ -8,22 +8,69 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
 
-    private static String DB_NAME = "Lib_Manager";
-    private static int VERSION = 1;
+    private static  final String DB_NAME = "Lib_Manager";
+    private static final int VERSION = 1;
 
+    static final String CREATE_TABLE_THU_THU = "CREATE TABLE ThuThu (" +
+            "    maTT    TEXT PRIMARY KEY," +
+            "    hoTen   TEXT NOT NULL," +
+            "    matKhau TEXT NOT NULL);";
+    //
+    static final String CREATE_TABLE_THANH_VIEN = "CREATE TABLE ThanhVien (" +
+            "    maTV    TEXT PRIMARY KEY," +
+            "    hoTen   TEXT NOT NULL," +
+            "    namSinh TEXT NOT NULL)";
+    //
+    static final String CREATE_TABLE_LOAI_SACH = "CREATE TABLE LoaiSach (" +
+            "    maLoai  INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "    tenLoai TEXT    NOT NULL)";
+    //
+    static final String CREATE_TABLE_SACH = "CREATE TABLE Sach (" +
+            "    maSach  INTEGER  PRIMARY KEY AUTOINCREMENT," +
+            "    tenSach TEXT     NOT NULL," +
+            "    giaThue INTERGER NOT NULL," +
+            "    maLoai  INTEGER  REFERENCES LoaiSach (maLoai) )";
+    //
+    static final String CREATE_TABLE_PHIEU_MUON = "CREATE TABLE PhieuMuon (" +
+            "    maPM     INTEGER  PRIMARY KEY AUTOINCREMENT," +
+            "    maTT     TEXT     REFERENCES ThuThu (maTT)," +
+            "    maTV     TEXT     REFERENCES ThanhVien (maTV)," +
+            "    maSach   INTEGER  REFERENCES Sach (maSach)," +
+            "    tienThue INTEGER  NOT NULL," +
+            "    ngay     DATE     NOT NULL," +
+            "    traSach  INTERGER NOT NULL)";
     public DbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DB_NAME, null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
         //Tao database o day
+        db.execSQL(CREATE_TABLE_THU_THU);
 
+        db.execSQL(CREATE_TABLE_THANH_VIEN);
+
+        db.execSQL(CREATE_TABLE_LOAI_SACH);
+
+        db.execSQL(CREATE_TABLE_SACH);
+
+        db.execSQL(CREATE_TABLE_PHIEU_MUON);
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String dropTableThuThu = "drop table if exists ThuThu";
+        db.execSQL(dropTableThuThu);
+        String dropTableThanhVien = "drop table if exists ThanhVien";
+        db.execSQL(dropTableThanhVien);
+        String dropTableLoaiSach = "drop table if exists LoaiSach";
+        db.execSQL(dropTableLoaiSach);
+        String dropTableSach = "drop table if exists Sach";
+        db.execSQL(dropTableSach);
+        String dropTablePhieuMuon = "drop table if exists PhieuMuon";
+        db.execSQL(dropTablePhieuMuon);
 
+        onCreate(db);
     }
 }
